d6c9832a47d06c36a75e7a80e10e5af18bfd1f9b
jason123s <hnguyen1193s@gmail.com>
2015-10-24 18:43:27 -0400

    Initialize the Screen

---
+++ MiniMinecraft/MiniMinecraft/MiniMinecraft/Dust.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniMinecraft 
+{
+    class Dust : Microsoft.Xna.Framework.Game
+    {
+        public Dust(){}
+
+        public void DrawDust()
+        {
+
+        }
+    }
+}
---
+++ MiniMinecraft/MiniMinecraft/MiniMinecraft/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MiniMinecraft
+{
+    class Player : Game1
+    {   
+        Texture2D playerTexture;
+        Rectangle playerHolder;
+        SpriteBatch spriteBatch;
+
+        public Player () {
+            this.playerTexture = Content.Load<Texture2D>("Player");
+            this.playerHolder = new Rectangle(10, 100, 30, 50);
+        }
+
+        public virtual void PlayerSetTexture(Texture2D texture) {
+            this.playerTexture = texture;
+        }
+
+        public virtual Texture2D playerGetTexture()
+        {
+            return this.playerTexture;
+        }
+
+        public virtual void PlayerSetRectangle(Rectangle rectangle)
+        {
+            this.playerHolder = rectangle;
+        }
+
+        public virtual Rectangle playerGetRectangle()
+        {
+            return this.playerHolder;
+        }
+
+    }
+}
